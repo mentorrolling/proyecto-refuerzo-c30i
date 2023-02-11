30bda6e7fa40bef89c146b0e817cf5065d6a8131
Pablo Marino <pmarino2013@gmail.com>
2023-02-11 16:51:35 -0300

    Asincronismo y DOM

--- js/app.js
+++ js/app.js
@@ -1,2 +1,53 @@
 const url =
   "https://newsapi.org/v2/top-headlines?country=ar&category=technology&apiKey=452d00247570407f893864ae69119af9";
+
+// fetch(
+//   "https://newsapi.org/v2/top-headlines?country=ar&category=technology&apiKey=452d00247570407f893864ae69119af9"
+// )
+//   .then((response) => response.json())
+//   .then((data) => console.log(data));
+
+//async await
+
+const noticias = [];
+
+const getNews = async () => {
+  const resp = await fetch(url);
+  const { articles } = await resp.json();
+
+  return articles;
+};
+
+getNews()
+  .then((respuesta) => {
+    noticias.push(...respuesta);
+    localStorage.setItem("noticias", JSON.stringify(noticias));
+
+    updateCards();
+  })
+  .catch((error) => console.warn(error));
+
+//cargar noticias
+const updateCards = () => {
+  const contenedor = document.querySelector("#container-cards");
+  contenedor.innerHTML = "";
+
+  noticias.map((noticia) => {
+    const col = document.createElement("div");
+    col.className = "col";
+
+    const tarjeta = `<div class="card h-100">
+      <img src="${
+        noticia.urlToImage ? noticia.urlToImage : "../assets/not_found.png"
+      }" class="card-img-top card-img" alt="${noticia.title}">
+      <div class="card-body">
+        <h5 class="card-title">${noticia.title}</h5>
+        <p class="card-text">${noticia.description}</p>
+      </div>
+    </div>`;
+
+    col.innerHTML = tarjeta;
+
+    contenedor.append(col);
+  });
+};
